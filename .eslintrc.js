ba2458d50ebc9e9fecd03b597daed082042309b4
ITWhiteRaccoon <eduardo.a@edu.pucrs.br>
2020-07-27 22:27:09 -0300

    Updates

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	extends: [
-		'plugin:vue/base'
-	],
-	rules: {
-		'vue/script-indent': ['error', 'tab', {'baseIndent': 1, 'switchCase': 1}],
-		'semi': ['error', 'always']
-	}
-};
