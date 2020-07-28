4b2d84f26d417f4e2aab5c0f605e6e3ff04d9377
Eduardo C. Andrade <eduardo.a@edu.pucrs.br>
2020-07-28 04:04:24 -0300

    alignment

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,33 @@
 <template>
 	<v-app>
 		<v-container>
-			<v-card>
-				<v-card-title>
-					Desculpe o transtorno :(
-				</v-card-title>
-				<v-card-text>
-					Estou trabalhando para trazer esta página para você o mais rápido possível.
-					<br>
-					Enquanto isso, entre em contato comigo!
-				</v-card-text>
-				<v-card-actions>
-					<v-btn small href="mailto:contact@eduandrade.dev">
-						<v-icon>{{mdiEmail}}</v-icon>
-					</v-btn>
-					<v-btn small href="https://github.com/ITWhiteRaccoon/">
-						<v-icon>{{mdiGithub}}</v-icon>
-					</v-btn>
-					<v-btn small href="https://linkedin.com/in/eduardocandrade/">
-						<v-icon>{{mdiLinkedin}}</v-icon>
-					</v-btn>
-				</v-card-actions>
-			</v-card>
+			<v-row justify="center">
+				<v-col sm="11" md="10" lg="8" xl="6">
+					<v-card>
+						<v-card-title>
+							Desculpe o transtorno :(
+						</v-card-title>
+						<v-card-text>
+							Estou trabalhando para trazer esta página para você o mais rápido possível.
+							<br>
+							Enquanto isso, entre em contato comigo!
+						</v-card-text>
+						<v-card-actions>
+							<v-spacer></v-spacer>
+							<v-btn small href="mailto:contact@eduandrade.dev">
+								<v-icon>{{mdiEmail}}</v-icon>
+							</v-btn>
+							<v-btn small href="https://github.com/ITWhiteRaccoon/">
+								<v-icon>{{mdiGithub}}</v-icon>
+							</v-btn>
+							<v-btn small href="https://linkedin.com/in/eduardocandrade/">
+								<v-icon>{{mdiLinkedin}}</v-icon>
+							</v-btn>
+							<v-spacer></v-spacer>
+						</v-card-actions>
+					</v-card>
+				</v-col>
+			</v-row>
 			<v-row>
 				<v-col>
 					<coding-pidgeons></coding-pidgeons>
@@ -32,8 +38,8 @@
 </template>
 
 <script>
-	import {mdiEmail, mdiGithub, mdiLinkedin} from '@mdi/js';
 	import CodingPidgeons from '@/components/coding-pidgeons';
+	import {mdiEmail, mdiGithub, mdiLinkedin} from '@mdi/js';
 
 	require('typeface-quicksand');
 	export default {
